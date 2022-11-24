3f4fdaecbcad5805465d14d477c39b5232a9a9b4
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2022-11-24 15:31:39 +0300

    sum

---
+++ CODE_FORCES/Sum.py
@@ -0,0 +1,16 @@
+# https://codeforces.com/contest/1742/problem/A
+def solution(arr):
+    arr.sort()
+    if arr[len(arr) - 1] == arr[0] + arr[1]:
+        print('YES')
+    else:
+        print('NO')
+
+
+testcases = int(input())
+cases = []
+for case in range(testcases):
+    x = [int(x) for x in input().split()]
+    cases.append(x)
+for case in cases:
+    solution(case)
