43fdbd2f97614ab70f374b51a958e76fb89c756c
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2022-12-17 19:21:03 +0300

    failed

---
+++ CODE_FORCES/Katryoshka.py
@@ -0,0 +1,35 @@
+# https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/G
+
+e =0
+m=222
+b=322
+
+def sol (e , m ,b ):
+    count = 0
+    while True:
+
+        if e >= 1 and m >= 1 and b >= 1:
+            count += 1
+            e -= 1
+            b -= 1
+            m -= 1
+
+        elif e >= 2 and b >= 1:
+            count += 1
+            e -= 2
+            b -= 1
+
+        elif e >= 2 and m >= 1 and b >= 1:
+            count += 1
+            e -= 2
+            b -= 1
+            m -= 1
+        else:
+            break
+        if e <= 0:
+            break
+    return count
+e , m ,b = [int(x) for x in input().split()]
+print(sol(e , m ,b ))
+
+
