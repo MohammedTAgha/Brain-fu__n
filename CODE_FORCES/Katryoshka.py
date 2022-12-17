ee6c0037b83bcf042a1939b2aabab9f47a9e8d45
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2022-12-17 19:50:32 +0300

    ...
    Time limit exceeded on test 124

--- CODE_FORCES/Katryoshka.py
+++ CODE_FORCES/Katryoshka.py
@@ -1,19 +1,25 @@
 # https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/G
 
-e =0
-m=222
-b=322
+e = 0
+m = 222
+b = 322
 
-def sol (e , m ,b ):
+def sol(e, m, b):
     count = 0
     while True:
-
+        if e <= 0 and b == 0:
+            break
         if e >= 1 and m >= 1 and b >= 1:
-            count += 1
-            e -= 1
-            b -= 1
-            m -= 1
-
+            if(e>m and b>m):
+                count +=m
+                e -= m
+                b -= m
+                m -= m
+            else:
+                count += 1
+                e -= 1
+                b -= 1
+                m -= 1
         elif e >= 2 and b >= 1:
             count += 1
             e -= 2
@@ -26,10 +32,7 @@ def sol (e , m ,b ):
             m -= 1
         else:
             break
-        if e <= 0:
-            break
     return count
-e , m ,b = [int(x) for x in input().split()]
-print(sol(e , m ,b ))
 
-
+e, m, b = [int(x) for x in input().split()]
+print(sol(e, m, b))
