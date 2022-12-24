b02c7e2b69303b6c960441071fb1e9b875e27520
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2022-12-24 10:01:48 +0300

    c++

--- CODE_FORCES/Recursion/Max Number.py
+++ CODE_FORCES/Recursion/Max Number.py
@@ -3,13 +3,15 @@
 #     return arr[0]
 #   max_of_rest = find_max_recursive(arr[1:])
 #   return max(arr[0], max_of_rest)
-m = float('-inf')
-
-print(1 < m)
-a = [1, -3, 5, 4, -6]
-
-
-def max(a, i):
-    if i < len(a):
-        if a[i] > max(a, i + 1):
-            return a[i]
+# m = float('-inf')
+#
+# print(1 < m)
+# a = [1, -3, 5, 4, -6]
+#
+#
+# def max(a, i):
+#     if i < len(a):
+#         if a[i] > max(a, i + 1):
+#             return a[i]
+a, b = [int(x) for x in input().split()]
+print(a+b)
---
+++ CODE_FORCES/inputfunctions.py
@@ -0,0 +1,12 @@
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+
+x=insr()
+print(x)
